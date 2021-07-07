701c5e5394c0e9bb78b1b5f777d22f95afd421a3
namseungsik <tmd7031@naver.com>
2021-07-07 22:24:46 +0900

    상품 서비스 HTML

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,4 +10,9 @@ public class ItemServiceApplication {
         SpringApplication.run(ItemServiceApplication.class, args);
     }
 
+    /**
+     *  인텔리제이 이슈
+     *  css 갖다 붙이고 다시 실행했을 때 접근 안 되면 out 디렉토리 날리고 다시 서버띄우기
+     */
+
 }
